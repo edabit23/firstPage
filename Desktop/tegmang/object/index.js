3cbfa282d1f9eef35572e5f0a95611d2c05219c6
edabit23 <edabitedabit0@gmail.com>
2023-10-12 18:06:43 +0500

    first

---
+++ Desktop/tegmang/object/index.js
@@ -0,0 +1,28 @@
+
+
+
+
+//   let codes = {
+//     "49": "Germany",
+//     "41": "Switzerland",
+//     "44": "Great Britain",
+//     // ..,
+//     "1": "USA"
+//   };
+
+//   for (let code in codes) {
+//     console.log(code);
+//   }
+
+
+let user = {
+    name: "John",
+    age: 30,
+    sayHi() {
+      // "this" is the "current object"
+      console.log(this.name);
+      console.log(user.name);
+    }
+  
+  };
+  user.sayHi(); // John
